c7c9af41dfcd902ce7339ca2e95a30f9c7648ce6
Ravikumaran-Elango <kuttyravikumaran@gmail.com>
2024-03-04 16:24:26 +0530

    first Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import List from './todolist/todo';
+
+function App() {
+  return (
+    <div>
+      <List/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/todolist/draft.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState: { items: [] },
+  reducers: {
+    addTodo: (state, action) => {
+      state.items.push(action.payload);
+    },
+    removeTodo: (state, action) => {
+      state.items = state.items.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    todos: todoSlice.reducer,
+  },
+});
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+
+const TodoList = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos.items);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== "") {
+      dispatch(addTodo({ id: Date.now(), text: newTodo }));
+      setNewTodo("");
+    }
+  };
+
+  const handleRemoveTodo = (id) => {
+    dispatch(removeTodo(id));
+  };
+
+  return (
+    <div>
+        <div>
+            <h1>To-Do List </h1>
+
+            <ul>
+              {todos.map((todo) => (
+                <li key={todo.id}>
+                  {todo.text}
+                  <button onClick={() => handleRemoveTodo(todo.id)}>remove</button>
+                </li>
+              ))}
+            </ul>
+
+            <div>
+              <input
+                type="text"
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+              />
+              <button onClick={handleAddTodo}>Add Todo</button>
+            </div>
+      </div>
+    </div>
+  );
+};
+
+const List = () => {
+  return (
+    <Provider store={store}>
+      <TodoList />
+    </Provider>
+  );
+};
+
+export default List;
